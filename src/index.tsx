bbd53ca8dd1079dc6d3ba81bf440f7a7e9400bc7
Philippe Aka <hello@philippeaka.com>
2022-04-24 19:38:04 -0400

    adding a changeset

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,5 @@
 import * as React from 'react';
 
-// Delete me
 export const Thing = () => {
-  return <div>the snozzberries taste like snozzberries</div>;
+  return <div>adding a changeset</div>;
 };
